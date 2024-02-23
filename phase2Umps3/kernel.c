cafda5fcf41180d7a6b451fc4cc57490a8b198ec
zBarba03 <ocram203@gmail.com>
2024-02-23 10:11:48 +0100

    started kernel init

--- phase2Umps3/kernel.c
+++ phase2Umps3/kernel.c
@@ -1,8 +1,19 @@
-#include "phase2Umps3/const.h"
+#include "headers/const.h"
 #include "phase1/pcb.c"
 
 int main(){
-	// inizializzazione
+	// GLOBAL VARS
+
+	// integer indicating the number of started, but not yet terminated processes.
+	int processCount;
+	//This integer is the number of started, but not terminated processes
+	//that in are the “blocked” state due to an I/O or timer request.
+	int softBlockCount;
+	
+	//Ready Queue
+	//Current Process
+	//Blocked PCBs
+
 
 	while(1){
 		// scheduler
