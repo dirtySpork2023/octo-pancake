2f57dbceba53ac83c27d921d195eeb4083d7e872
zBarba03 <ocram203@gmail.com>
2024-08-14 14:00:07 +0200

    sst progress

--- phase3Umps3/initProc.c
+++ phase3Umps3/initProc.c
@@ -1,5 +1,9 @@
 //phase 3 global variables
-
+// instantiatorProcess?
 void test(){
 	initSwapStructs();
+	// each peripheral device
+	// eight SST for each U-proc
+
+	// terminate after all SST have terminated
 }
--- phase3Umps3/sst.c
+++ phase3Umps3/sst.c
@@ -1,9 +1,45 @@
+#include "headers/sst.h"
+
 // system service thread
-void SST(){
-	// initialize the corresponding U-proc
+void SST(memaddr func){
+	state_t childState;
+	support_t childSupport;
 	pcb_PTR child_pcb;
 	
-	
+	// initialize the corresponding U-proc that will execute func
+    STST(&childState);
+    childState.reg_sp = childState.reg_sp - QPAGE;
+    childState.pc_epc = func;
+    childState.status |= IEPBITON | CAUSEINTMASK | TEBITON;
+
+	/*
+ support_t {
+    int        sup_asid;                        process ID
+    state_t    sup_exceptState[2];              old state exceptions
+    context_t  sup_exceptContext[2];            new contexts for passing up
+    pteEntry_t sup_privatePgTbl[USERPGTBLSIZE]; user page table
+    struct list_head s_list;
+} support_t;
+
+	// TODO initialize support struct
+	childSupport.sup_asid
+	childSupport.sup_exceptState[2]
+	childSupport.sup_exceptContext[2]
+	childSupport.sup_privatePgTbl[USERPGTBLSIZE]
+	*/
+
+	ssi_create_process_t ssi_create_process = {
+        .state = &childState,
+        .support = &childSupport,
+    };
+    ssi_payload_t payload = {
+        .service_code = CREATEPROCESS,
+        .arg = &ssi_create_process,
+    };
+    SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb, (unsigned int)&payload, 0);
+    SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb, (unsigned int)(&child_pcb), 0);
+
+
 	// wait for service requests and manage them
 	sst_payload_t payload = NULL;
 	unsigned int answer;
@@ -52,7 +88,6 @@ void terminate(){
     SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb, (unsigned int)(&term_process_payload), 0);
     SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb, 0, 0);
 }
-
 
 /*
 sst_print_t print_payload = {
